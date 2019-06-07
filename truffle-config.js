f7f45016265d53da3bc69d3d39a684c0ad22ffd7
Michael Carter <MCarter@ITSRND.COM>
2019-06-07 12:59:34 -0700

    changes

--- truffle-config.js
+++ truffle-config.js
@@ -2,18 +2,21 @@ const path = require("path");
 require('dotenv').config();
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
+const MNEMONIC = process.env.MNEMONIC;
+
+
 module.exports = {
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(process.env.MNEMONIC, process.env.ROPSTEN_URL);
+        return new HDWalletProvider(MNEMONIC, process.env.ROPSTEN_URL);
       },
       network_id: '3',
     },
     test: {
       provider: function() {
-        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:9545/");
+        return new HDWalletProvider(MNEMONIC, "http://127.0.0.1:9545/");
       },
       network_id: '*',
     },
