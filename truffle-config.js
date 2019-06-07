382c39559a3ec8382c0edf8ae4be6e0855d0c6d0
Michael Carter <MCarter@ITSRND.COM>
2019-06-07 13:19:50 -0700

    changes

--- truffle-config.js
+++ truffle-config.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
 const MNEMONIC = process.env.MNEMONIC;
-
+const INFURA_API = process.env.INFURA_API;
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(MNEMONIC, process.env.ROPSTEN_URL);
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/"+INFURA_API);
       },
       network_id: '3',
     },
@@ -21,5 +21,4 @@ module.exports = {
       network_id: '*',
     },
   }
-
 };
